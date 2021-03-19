a4782b261daf6585357a1c991459e15d3e09ea45
Алексей Валерьевич Белоусов <chiisai@yandex.ru>
2021-03-19 23:50:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SF5._2._8
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}: ", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static int[] SortArray()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
